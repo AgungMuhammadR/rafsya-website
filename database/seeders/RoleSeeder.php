b11c08fde4d2d433f0c81fa160a73e2cb4aa6e87
yeremi777 <kurikarakenjou@gmail.com>
2021-12-20 10:32:18 +0700

    add migrations & registration

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'role' => 'Architect'
+        ]);
+        Role::create([
+            'role' => 'Customer'
+        ]);
+    }
+}
