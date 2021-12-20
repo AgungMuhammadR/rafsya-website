cc008e2ea52bd5ac934c511f0f6f9ccacf1eedd2
yeremi777 <kurikarakenjou@gmail.com>
2021-12-20 17:02:39 +0700

    add categories table

---
+++ database/migrations/2021_12_20_095025_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::table('designs', function (Blueprint $table) {
+            $table->foreignId('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+
+        Schema::table('designs', function (Blueprint $table) {
+            $table->dropColumn('category_id');
+        });
+    }
+}
