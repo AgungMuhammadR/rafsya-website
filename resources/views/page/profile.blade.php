d4bbeaa6253900b311dfeee6b0a2f61f8b8f96a6
yeremi777 <kurikarakenjou@gmail.com>
2021-12-21 17:09:26 +0700

    add function: update profile

--- resources/views/page/profile.blade.php
+++ resources/views/page/profile.blade.php
@@ -25,6 +25,8 @@
                     <div class="kategori">
                         <div class="row mt-12" style="width: 68rem;">
                             <div class="col-sm-12">
+                                <h1>Id : {{ auth()->user()->id }}</h1>
+
                                 @if (session()->has('success'))
                                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                                         {{ session('success') }}
@@ -33,37 +35,52 @@
                                     </div>
                                 @endif
 
-                                <form class="row g-12 mt-5" style="background-color: #F7F8FA;">
+                                <form action="profile/update" class="row g-12 mt-5" style="background-color: #F7F8FA;"
+                                    method="POST">
+                                    @method('PUT')
+                                    @csrf
                                     <div class="col-6 mt-4">
-                                        <label for="inputName" class="form-label font-weight-bold">Username</label>
-                                        <input type="email" class="form-control" id="inputName" placeholder="esthera">
+                                        <label for="username" class="form-label font-weight-bold">Username</label>
+                                        <input type="text" name="username"
+                                            class="form-control @error('username') is-invalid @enderror" id="username"
+                                            placeholder="agungmr07">
                                     </div>
                                     <div class="col-6 mt-4">
-                                        <label for="inputCity" class="form-label font-weight-bold">City</label>
-                                        <input type="password" class="form-control" id="inputCity"
-                                            placeholder="Jakarta Selatan">
+                                        <label for="province" class="form-label font-weight-bold">Province</label>
+                                        <select name="province" class="form-control" id="province">
+                                            <option id="select-province" value="" selected>Select Province</option>
+                                            @foreach ($provinces as $province)
+                                                <option value="{{ $province->id }}">{{ $province->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
                                     </div>
                                     <div class="col-6 mt-4">
-                                        <label for="inputAddress" class="form-label font-weight-bold">Email</label>
-                                        <input type="text" class="form-control" id="inputAddress"
-                                            placeholder="esthera@simmmple.com">
+                                        <label for="email" class="form-label font-weight-bold">Email</label>
+                                        <input type="email" name="email"
+                                            class="form-control @error('email') is-invalid @enderror" id="email"
+                                            placeholder="agungambisbanget@gmail.com">
                                     </div>
                                     <div class="col-6 mt-4">
-                                        <label for="inputCounty" class="form-label font-weight-bold">country</label>
-                                        <input type="text" class="form-control" id="inputCounty" placeholder="Indonesia">
+                                        <label for="city" class="form-label font-weight-bold">City</label>
+                                        <select name="city" class="form-control" id="city" onchange="idcity(this.value)">
+                                        </select>
                                     </div>
                                     <div class="col-6 mt-4">
-                                        <label for="inputNumber" class="form-label font-weight-bold">Countact Number</label>
-                                        <input type="text" class="form-control" id="inputNumber"
-                                            placeholder="0842-1746-1143">
+                                        <label for="phone_number" class="form-label font-weight-bold">Contact Number</label>
+                                        <input type="text" name="phone_number"
+                                            class="form-control @error('phone_number') is-invalid @enderror"
+                                            id="phone_number" placeholder="0842-1746-1143">
                                     </div>
                                     <div class="col-6 mt-4">
-                                        <label for="inputPassword" class="form-label font-weight-bold">Password</label>
-                                        <input type="text" class="form-control" id="inputPassword" placeholder="">
+                                        <label for="password" class="form-label font-weight-bold">Password</label>
+                                        <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                            name="password" id="inputPassword" placeholder="">
                                     </div>
                                     <div class="col-md-12 mt-4">
-                                        <label for="inputAddress" class="form-label font-weight-bold">Addresss</label>
-                                        <input type="text" class="form-control" id="inputAdddress"
+                                        <label for="address" class="form-label font-weight-bold">Address</label>
+                                        <input type="text" name="address"
+                                            class="form-control @error('address') is-invalid @enderror" id="inputAdddress"
                                             placeholder="Jl. Kemang Utara C No 42">
                                     </div>
                                     <div class="col-12 mt-4 mb-5 text-md-right">
@@ -78,3 +95,31 @@
         </div>
     </div>
 @endsection
+
+@push('js')
+    <script>
+        $('form').each(function() {
+            this.reset()
+        });
+
+        $('#province').change(function() {
+            $("#province option[id='select-province']").hide()
+            $.ajax({
+                type: 'GET',
+                url: '/profile/cities/' + $(this).val(),
+                success: function(data) {
+                    $('#city').empty().append(
+                        data.map(function(item) {
+                            return `<option value="${item.id}">${item.name}</option>`
+                        })
+                    )
+                },
+                error: function(xhr, textStatus, errorThrown) {
+                    console.log('XHR', xhr);
+                    console.log('status', textStatus);
+                    console.log('Error in', errorThrown);
+                }
+            });
+        });
+    </script>
+@endpush
