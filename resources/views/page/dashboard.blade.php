3930eed6a432422f34880b69cfb6533a6535e274
unknown <rizkiagung9918@gmail.com>
2022-01-04 00:53:31 +0700

    add dasboard arsitek

---
+++ resources/views/page/dashboard.blade.php
@@ -0,0 +1,122 @@
+@extends('layout.main')
+
+@section('container')
+    <div class="content mt-5" style="padding-left: 65px;">
+        <div class="profile">
+            <div class="row">
+                <div class="col-md-2 bg-transparent">
+                    <div class="card-body">
+                        <a href="{{ url('profile') }}" class="nav-link">
+                            <h4 class="profile" style="color: #828599;">Profil</h4>
+                        </a>
+                        <a href="{{ url('profile/product') }}" class="nav-link">
+                            <h4 class="produk" style="color: #828599;">Product</h4>
+                        </a>
+                        <a href="" class="nav-link">
+                            <h4 class="pesanan" style="color: #1ACBAA;">Dashboard</h4>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-md-10">
+                    
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h1 style="color: #002678;"> Hallo, Frans </h1>
+                            <p> Selamat datang di dashboard Anda ! </p>
+                            <div class="card">
+                            <div class="card-body">
+                                <h5 class="font-weight-bold mb-3 text-center">Produk Terjual</h5>
+                                </br> </br>
+                                <h1 class="font-weight-bold mb-3 text-center" > </b> 120 </h1>
+                                </br> </br>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            </br> </br> </br> </br>
+                            <div class="card">
+                            <div class="card-body">
+                                <h5 class="font-weight-bold mb-3 text-center">Semua Produk</h5>
+                                </br> </br>
+                                <h1 class="font-weight-bold mb-3 text-center" > </b> 220 </h1>
+                                </br> </br>
+                            </div>
+                            </div>
+                            </br> </br>
+                        </div>
+                        </br> </br>
+                        <div class="col-sm-5">
+                            </br> </br> </br> </br>
+                            <div class="card text-center">
+                            <div class="card-body">
+                                <h5 class="font-weight-bold mb-3 text-center">Your Earning this month </h5>
+                                </br> </br>
+                                <h2 class="font-weight-bold mb-3 text-center"> Rp. 18.000.000,00 </h2>
+                                </br>
+                                <button type="button" class="btn btn-outline-primary">Tarik Penghasilan Anda</button>
+                            </div>
+                            </div>
+                        </div>
+                        </br> </br>
+                    </div>
+                    
+                    <div class="card">
+                            <table id="shoppingCart" class="table table-condensed table-responsive">
+                                <thead>
+                                    <tr>
+                                        <th style="width:65%">Produk</th>
+                                        <th style="width:30%">Tanggal</th>
+                                        <th style="width:5%;text-align: right;">Total</th>
+                                    </tr>
+                                </thead>
+                                
+                                <tbody>
+                                    <tr>
+                                        <h5> List Produk Terjual </h5>
+                                        <td data-th="Product">
+                                            <div class="row">
+                                                <div class="col-md-3 text-left">
+                                                    <img src="images/kategori4.png" alt=""
+                                                        class="img-fluid d-none d-md-block rounded mb-2 shadow ">
+                                                </div>
+                                                <div class="col-md-9 text-left mt-sm-2">
+                                                    <h6>Rumah minimalis modern</h6>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td data-th="Date">22 januari 2021</td>
+                                        <td class="actions" data-th="">
+                                            <div class="text-right">
+                                                <p>Rp.1.299.000</p>
+                                            </div>
+                                        </td>  
+                                    </tr>
+                                    <tr>
+                                        <td data-th="Product">
+                                            <div class="row">
+                                                <div class="col-md-3 text-left">
+                                                    <img src="images/kategori4.png" alt=""
+                                                        class="img-fluid d-none d-md-block rounded mb-2 shadow ">
+                                                </div>
+                                                <div class="col-md-9 text-left mt-sm-2">
+                                                    <h6>Rumah minimalis classic</h6>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td data-th="Date">27 februari 2021</td>
+                                        <td class="actions" data-th="">
+                                            <div class="text-right">
+                                                <p>Rp.9.299.000</p>
+                                            </div>
+                                        </td>  
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
